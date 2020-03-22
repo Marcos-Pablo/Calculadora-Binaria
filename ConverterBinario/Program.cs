80790b6a14a51c0771d6f334fc228d587f4b1c32
Marcos Almeida <monitoria.puccontagem@gmail.com>
2020-03-21 21:39:38 -0300

    First Commit

---
+++ ConverterBinario/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConverterBinario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            Console.Write("Digite um número: ");
+            num = int.Parse(Console.ReadLine());
+            string resultado = converterParaDecimal(num);
+
+            Console.WriteLine(resultado);
+            Console.ReadKey();
+        }
+
+        static string converterParaDecimal(int num)
+        {
+            string valor = "";
+            if (num == 0 || num == 1)
+                valor = Convert.ToString(num);
+            else
+            {
+                while (num > 0)
+                {
+                    valor += Convert.ToString(num % 2);
+                    num /= 2;
+                }
+            }
+
+            return inverterString(valor);
+        }
+
+        static string inverterString(string texto)
+        {
+            string textoInvertido = "";
+            for (int i = texto.Length - 1; i >= 0; i--)
+            {
+                textoInvertido += texto[i];
+            }
+            return textoInvertido;
+        }
+    }
+}
---
+++ ConverterBinario/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associada a um assembly.
+[assembly: AssemblyTitle("ConverterBinario")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConverterBinario")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("687b1e76-2dd1-42ae-b786-15eae850d6c7")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// utilizando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
